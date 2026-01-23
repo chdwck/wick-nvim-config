595d6322892f39ce18a7253304279f3dde29e0ce
Chadwick Platt-Kuhn <chadwick@wicksworld.dev>
2026-01-22 21:13:46 -0800

    no transparent background

--- lua/chdwck/lazy.lua
+++ lua/chdwck/lazy.lua
@@ -223,7 +223,7 @@ require("lazy").setup({
 			-- Optionally configure and load the colorscheme
 			-- directly inside the plugin declaration.
 			vim.g.gruvbox_material_enable_italic = true
-			vim.g.gruvbox_material_transparent_background = "1"
+			-- vim.g.gruvbox_material_transparent_background = "1"
 			vim.g.gruvbox_material_background = "hard"
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
